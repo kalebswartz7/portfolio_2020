e8daeb479d3701dddf760683d4d11b1e09ab6a7c
kalebswartz7 <kalebswartz7@gmail.com>
2020-06-23 16:51:52 -0400

    Start js file

---
+++ static/js/portfolio.js
@@ -0,0 +1,6 @@
+console.log('hi');
+var showcaseAfterHeight = document.getElementById('myVideo').clientHeight;
+document.getElementById('showcase-container').style.height = showcaseAfterHeight;
+
+var showcaseAfterWidth = document.getElementById('myVideo').clientWidth;
+document.getElementById('showcase-container').style.width = showcaseAfterWidth;
