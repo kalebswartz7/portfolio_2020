56118059dbe6ff9cc610152a895ee216dbf04ce2
kalebswartz7 <kalebswartz7@gmail.com>
2020-07-08 11:20:55 -0400

    add viewport meta tag, remove js

--- static/js/portfolio.js
+++
@@ -1,6 +0,0 @@
-var videoContainerHeight = document.getElementById('myVideo').clientHeight;
-document.getElementById('video-container').style.height = videoContainerHeight;
-
-var videoContainerWidth = document.getElementById('myVideo').clientWidth;
-document.getElementById('video-container').style.width = videoContainerWidth;
-
